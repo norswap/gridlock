0c990d6ea1161c348844a680d3345dd8b17a0dc5
0xhatsume <0xhatsume@gmail.com>
2024-03-23 18:00:39 +0800

    :sparkles: Add burnable for 1155

--- packages/contracts/src/GridResource1155.sol
+++ packages/contracts/src/GridResource1155.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "solmate/tokens/ERC1155.sol";
 import "solmate/auth/Owned.sol";
+import "openzeppelin/utils/Strings.sol";
 
 contract GridResource1155 is ERC1155, Owned
 {
@@ -20,6 +21,18 @@ contract GridResource1155 is ERC1155, Owned
     constructor(string memory _baseURI) Owned(msg.sender) {
         baseURI = _baseURI;
     }
+
+    // ===== View Funcs =====
+
+    // Override the uri function to define how token URIs are determined
+    function uri(uint256 id) public view override returns (string memory) {
+        // Implement your logic to return the URI for token id
+        
+        //return string(abi.encodePacked(baseURI, Strings.toString(id), ".json"));
+        return string(abi.encodePacked(baseURI, Strings.toString(id)));
+    }
+
+    // ===== Setter Funcs =====
 
     // Allows the contract owner to set a new base URI
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
@@ -46,12 +59,16 @@ contract GridResource1155 is ERC1155, Owned
         _batchMint(to, ids, amounts, data);
     }
 
-    // Override the uri function to define how token URIs are determined
-    function uri(uint256 id) public view override returns (string memory) {
-        // Implement your logic to return the URI for token id
-        
-        //return string(abi.encodePacked(baseURI, Strings.toString(id), ".json"));
-        return string(abi.encodePacked(baseURI, Strings.toString(id)));
+    // Function to burn a single type of token
+    function burn(address account, uint256 id, uint256 amount) public {
+        require(account == msg.sender || isApprovedForAll(account, msg.sender), "Caller is not owner nor approved");
+        _burn(account, id, amount);
+    }
+
+    // Function to burn multiple types of tokens in a single call
+    function burnBatch(address account, uint256[] memory ids, uint256[] memory amounts) public {
+        require(account == msg.sender || isApprovedForAll(account, msg.sender), "Caller is not owner nor approved");
+        _batchBurn(account, ids, amounts);
     }
 
 }
