a6318fac43969d5534db6f5086ce2c88d5566663
0xhatsume <0xhatsume@gmail.com>
2024-03-23 17:10:26 +0800

    :sparkles: Add base 721s and 1155s

--- packages/contracts/src/GridLand721.sol
+++ packages/contracts/src/GridLand721.sol
@@ -7,59 +7,34 @@ import "openzeppelin/utils/Strings.sol";
 
 contract GridLand721 is ERC721, Owned {
     string private baseURI;
-    uint256 public totalSupply;
-    uint256 public maxSupply = 25; // Max Land 5x5 square grid of lands
-
-    string[25] public landURI;
-
     constructor(
         string memory name, 
         string memory symbol, 
-        uint256 _max_supply
+        string memory _baseURI
     ) 
         ERC721(name, symbol) Owned(msg.sender) 
     {
-        //baseURI = _baseURI;
-        maxSupply = _max_supply;
+        baseURI = _baseURI;
     }
-
-    // ===== Internal Functions ===== 
-
-    
-
-    // ===== Setters =====
 
     // Function to set the base URI for metadata
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
     }
 
-    // Example function to mint a new token
-    function mint(address to, uint256 id) public onlyOwner {
-        require(totalSupply < maxSupply, "Max supply reached");
-        _mint(to, id);
-        totalSupply += 1; // Increment the total supply
+    // Override the _baseURI function to return the base URI
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
     }
-
-    function setTokenURI(uint256 id, string memory _uri) public onlyOwner {
-        require(ownerOf(id) != address(0), "Token does not exist.");
-        require(id < maxSupply, "Token ID hit max-supply.");
-        landURI[id] = _uri;
-    }
-
-    // ===== Getters =====
 
     // Override the tokenURI function to return the full URI for each token
     function tokenURI(uint256 id) public view override returns (string memory) {
         require(ownerOf(id) != address(0), "Token does not exist.");
-        require(id < maxSupply, "Token ID hit max-supply.");
-        //return string(abi.encodePacked(baseURI, Strings.toString(id)));
-        return landURI[id];
+        return string(abi.encodePacked(baseURI, Strings.toString(id)));
     }
 
-    // Custom view function to return the entire array.
-    function getAllLandURIs() public view returns (string[25] memory) {
-        return landURI;
+    // Example function to mint a new token
+    function mint(address to, uint256 id) public onlyOwner {
+        _mint(to, id);
     }
-
 }
